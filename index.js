13d300d06bab1e44f8e60e180b0cecf029260d20
faithwest <faithchepkoech425@gmail.com>
2023-09-18 13:10:44 +0300

    variables lab complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+// Code your solution in this filec!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+console.log(mostProfitableNeighborhood)
+let companyCeo = 'Susan Smith';
